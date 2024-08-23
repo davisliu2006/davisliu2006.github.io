87ebe553a20d50c5bb54881be6092c91e3550b17
Davis Liu <davisliu2006@gmail.com>
2024-08-23 23:17:58 +0800

    Update

--- src/load_global.js
+++ src/load_global.js
@@ -3,5 +3,21 @@ async function loadGlobal() {
     let text = await response.text();
     let global = document.getElementById("global");
     global.innerHTML = text;
+
+    // nav bar highlight
+    let navInfo = document.querySelector("._info #nav-info");
+    if (navInfo) {
+        let navBtn = document.getElementById(navInfo.innerHTML);
+        navBtn.classList.add("active");
+    }
+
+    // top bar path
+    let path = document.querySelector("#top-bar-path");
+    let pathInfo = document.querySelector("._info #path-info");
+    if (pathInfo) {
+        path.innerHTML = pathInfo.innerHTML;
+    } else {
+        path.innerHTML = "~/";
+    }
 }
 loadGlobal();
