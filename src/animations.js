9fa0e71d0c55fa9455bdaa8061cc88f148650dc5
Davis Liu <davisliu2006@gmail.com>
2024-12-16 18:35:49 -0500

    Animations

--- src/animations.js
+++ src/animations.js
@@ -13,19 +13,25 @@ function initViewPosAnim(selector, anim, start, end) {
     });
 }
 
-function initAnimInView(selector, anim, start, duration) {
+function initAnimInView(selector, anim, start, duration, anchor=0, delay=0, revert=false) {
     let elem = document.querySelector(selector);
     elem.style["animation-name"] = anim;
     elem.style["animation-duration"] = duration+"s";
     elem.style["animation-play-state"] = "paused";
+    elem.style["animation-delay"] = delay+"s";
     elem.style["animation-iteration-count"] = "1";
     elem.style["animation-fill-mode"] = "both";
 
     window.addEventListener("scroll", function() {
         let boundingRect = elem.getBoundingClientRect();
-        let pos = boundingRect.top/window.innerHeight;
+        let pos = (boundingRect.top*(1-anchor)+boundingRect.bottom*anchor)/window.innerHeight;
         if (pos <= start) {
+            elem.style["animation-name"] = anim;
             elem.style["animation-play-state"] = "running";
+        } else if (revert) {
+            elem.style["animation-name"] = "none";
+            elem.style["animation-play-state"] = "paused";
+            elem.style["animation-delay"] = delay+"s";
         }
     });
 }
