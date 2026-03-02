2007d13f17df485a73c2052956e5112b0b6e016d
Davis Liu <davisliu2006@gmail.com>
2026-03-02 18:07:01 -0500

    Wrap slideshows

--- src/slideshows.js
+++ src/slideshows.js
@@ -14,8 +14,17 @@ function initSlideshow(selector, wrap = true) {
     console.log(imgs.length);
     frame.style.width = "0px";
 
+    let indx = (wrap? 1 : 0);
 
     function setup() {
+        if (wrap) {
+            let firstClone = imgs[0].cloneNode(true);
+            let lastClone = imgs[imgs.length-1].cloneNode(true);
+            inner.append(firstClone);
+            inner.prepend(lastClone);
+            imgs = slide.querySelectorAll("img");
+        }
+
         let width = imgs[indx].clientWidth;
         let pos = [0];
         for (let i = 1; i < imgs.length; i++) {
@@ -24,7 +33,11 @@ function initSlideshow(selector, wrap = true) {
 
         frame.style.width = width+"px";
         inner.style.transform = "translateX("+(-pos[indx])+"px)";
-        pgCount.innerHTML = (indx+1)+"/"+imgs.length;
+        if (wrap) {
+            pgCount.innerHTML = (indx)+"/"+(imgs.length-2);
+        } else {
+            pgCount.innerHTML = (indx+1)+"/"+imgs.length;
+        }
     }
     function update() {
         let width = imgs[indx].clientWidth;
@@ -37,15 +50,54 @@ function initSlideshow(selector, wrap = true) {
         inner.style.transition = "transform 0.5s"
         frame.style.width = width+"px";
         inner.style.transform = "translateX("+(-pos[indx])+"px)";
-        pgCount.innerHTML = (indx+1)+"/"+imgs.length;
+        if (wrap) {
+            pgCount.innerHTML = (indx)+"/"+(imgs.length-2);
+        } else {
+            pgCount.innerHTML = (indx+1)+"/"+imgs.length;
+        }
+    }
+    function updateImmediate() {
+        let width = imgs[indx].clientWidth;
+        let pos = [0];
+        for (let i = 1; i < imgs.length; i++) {
+            pos[i] = pos[i-1]+imgs[i-1].clientWidth;
+        }
+
+        frame.style.transition = ""
+        inner.style.transition = ""
+        frame.style.width = width+"px";
+        inner.style.transform = "translateX("+(-pos[indx])+"px)";
+        if (wrap) {
+            pgCount.innerHTML = (indx)+"/"+(imgs.length-2);
+        } else {
+            pgCount.innerHTML = (indx+1)+"/"+imgs.length;
+        }
     }
                             
     lbtn.onclick = function() {
-        if (indx > 0) {indx--;}
+        if (wrap) {
+            if (indx > 1) {indx--;}
+            else {
+                indx = imgs.length-1;
+                updateImmediate();
+                indx--;
+            }
+        } else {
+            if (indx > 0) {indx--;}
+        }
         update();
     }
     rbtn.onclick = function() {
-        if (indx < imgs.length-1) {indx++;}
+        if (wrap) {
+            if (indx < imgs.length-2) {indx++;}
+            else {
+                indx = 0;
+                updateImmediate();
+                indx++;
+            }
+        } else {
+            if (indx < imgs.length-1) {indx++;}
+        }
         update();
     }
     window.addEventListener("load", function(event) {
